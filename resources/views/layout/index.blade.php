0cfe0f7327667770b36adfdeffd2fd83970e078a
AliAbdurohman16 <alyabdurohman16@gmail.com>
2022-11-24 14:02:10 +0700

    update index.blade

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -5,8 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 </head>
 <body>
     @yield('content')
+
+    <script src="{{ asset('assets/js/index.js') }}"></script>
 </body>
 </html>
