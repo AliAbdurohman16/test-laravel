b0761f1d5451490689356c86c1c307071f0aa049
AliAbdurohman16 <alyabdurohman16@gmail.com>
2022-11-24 15:00:08 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\CategoryController::class, 'index'])->name('/');
+Route::resources(['/' => App\Http\Controllers\CategoryController::class,]);
