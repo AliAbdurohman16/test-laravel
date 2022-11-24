9797e5a7485e484bf232e1c4354f14f9f72a8031
AliAbdurohman16 <alyabdurohman16@gmail.com>
2022-11-24 13:09:45 +0700

    add assets and list.blade.php

---
+++ resources/views/list.blade.php
@@ -0,0 +1,16 @@
+<input type="text" id="search" onkeyup="search()" placeholder="Search...">
+
+<table id="table-list">
+    <tr class="table-header">
+        <th style="width: 60%;">Name</th>
+        <th style="width: 40%;">Is Publish</th>
+    </tr>
+    <tr>
+        <td>Cek</td>
+        <td>True</td>
+    </tr>
+    <tr>
+        <td>Coba</td>
+        <td>False</td>
+    </tr>
+</table>
