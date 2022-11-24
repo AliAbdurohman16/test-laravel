aca1c5d9802c1bf2a4accad74e64c24314a072eb
AliAbdurohman16 <alyabdurohman16@gmail.com>
2022-11-24 17:18:18 +0700

    update list.blade and style.css

--- resources/views/category/list.blade.php
+++ resources/views/category/list.blade.php
@@ -3,30 +3,34 @@
 @section('title', 'List Category')
     
 @section('content')
-<a href="{{ 'create' }}">Add</a>
 <input type="text" id="search" onkeyup="search()" placeholder="Search...">
 
-<table id="table-list">
-    <thead>
-        <tr class="table-header">
-            <th style="width: 5%;">No</th>
-            <th style="width: 55%;">Name</th>
-            <th style="width: 20%;">Is Publish</th>
-            <th style="width: 20%;">#</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($category as $row)
-        <tr>
-            <td>{{ $loop->iteration }}</td>
-            <td>{{ $row->name }}</td>
-            <td>{{ $row->is_publish == 1 ? 'True' : 'False' }}</td>
-            <td>
-                <a href="{{ $row->id . '/edit' }}">Edit</a>
-                <a href="{{ $row->id }}">Delete</a>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+<div class="card">
+    <div class="container">
+        <a href="{{ 'create' }}" class="btn info">Add</a>
+        <table class="mt-3" id="table-list">
+            <thead>
+                <tr class="table-header">
+                    <th style="width: 5%;">No</th>
+                    <th style="width: 55%;">Name</th>
+                    <th style="width: 20%;">Is Publish</th>
+                    <th style="width: 20%;">#</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($category as $row)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $row->name }}</td>
+                    <td>{{ $row->is_publish == 1 ? 'True' : 'False' }}</td>
+                    <td>
+                        <a href="{{ $row->id . '/edit' }}" class="btn-sm success">Edit</a>
+                        <a href="{{ $row->id }}" class="btn-sm danger">Delete</a>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
 @endsection
