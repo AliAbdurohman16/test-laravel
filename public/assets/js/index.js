8c2967d7843d5b3ddee21115dd24b6db8ba29871
AliAbdurohman16 <alyabdurohman16@gmail.com>
2022-11-24 14:29:18 +0700

    update index.js

---
+++ public/assets/js/index.js
@@ -0,0 +1,27 @@
+function search() {
+        var input, filter, table, tr, name, is_publish, i, txtValue;
+    input = document.getElementById("search");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("table-list");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+        name = tr[i].getElementsByTagName("td")[1];
+        is_publish = tr[i].getElementsByTagName("td")[2];
+        if (name) {
+            txtValue = name.textContent || name.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        } if (is_publish) {
+            txtValue = is_publish.textContent || is_publish.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }            
+        }
+    }
+}
