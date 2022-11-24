8840dda009aa8d909c6ebffce96641941dd682be
AliAbdurohman16 <alyabdurohman16@gmail.com>
2022-11-24 13:33:55 +0700

    add CategoryController and update list.blade

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,16 +1,25 @@
 <input type="text" id="search" onkeyup="search()" placeholder="Search...">
 
 <table id="table-list">
-    <tr class="table-header">
-        <th style="width: 60%;">Name</th>
-        <th style="width: 40%;">Is Publish</th>
-    </tr>
-    <tr>
-        <td>Cek</td>
-        <td>True</td>
-    </tr>
-    <tr>
-        <td>Coba</td>
-        <td>False</td>
-    </tr>
+    <thead>
+        <tr class="table-header">
+            <th style="width: 5%;">No</th>
+            <th style="width: 55%;">Name</th>
+            <th style="width: 20%;">Is Publish</th>
+            <th style="width: 20%;">#</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($category as $row)
+        <tr>
+            <td>{{ $loop->iteration }}</td>
+            <td>{{ $row->name }}</td>
+            <td>{{ $row->is_publish }}</td>
+            <td>
+                <button>Edit</button>
+                <button>Delete</button>
+            </td>
+        </tr>
+        </tr>
+    </tbody>
 </table>
