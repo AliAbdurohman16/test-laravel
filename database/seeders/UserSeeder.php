f3540f32d5e80c882f6434e2b580206fb35e2364
AliAbdurohman16 <alyabdurohman16@gmail.com>
2022-11-24 12:14:00 +0700

    add Userseeder and update DatabaseSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::factory(1)->create();
+
+        \App\Models\User::insert([
+            'name' => 'User',
+            'email' => 'user@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345'),
+        ]);
+    }
+}
