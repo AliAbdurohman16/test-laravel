2b5e2fd6c7bd425c71d8b46530970e89519ac6e6
AliAbdurohman16 <alyabdurohman16@gmail.com>
2022-11-24 18:25:55 +0700

    add API/CategoryController.php

---
+++ app/Http/Controllers/API/CategoryController.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json([
+            "success" => true,
+            "message" => "Category List",
+            "data" => Category::all(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'is_publish' => 'required',
+        ]);
+
+        $category = Category::create($request->all());
+
+        return response()->json([
+            "success" => true,
+            "message" => "Category created successfully",
+            "data" => $category
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Category $category)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'is_publish' => 'required',
+        ]);
+
+        $category->update($request->all());
+
+        return response()->json([
+            "success" => true,
+            "message" => "Category updated successfully",
+            "data" => $category
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        return response()->json([
+            "success" => true,
+            "message" => "Category deleted successfully",
+            "data" => $category
+        ]);
+    }
+}
